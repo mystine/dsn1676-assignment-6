0cc58adb26b9af1e1f45dea1136b363e00e2f547
Mystine Laframboise Vien <mystinelaframboisevien@Mystines-MacBook-Pro.local>
2015-03-09 13:40:16 -0400

    changes in js - animations not working

--- js/main.js
+++ js/main.js
@@ -1,31 +1,34 @@
 var $btnShowHide = $('.btn-show-hide');
 var $box = $('.box');
-             
+
 var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
 
-var $btnCollapse = $('.btn-collapse-expand');
+var $btnExpand = $('.btn-collapse-expand');
 var $panel = $('.panel');
 
 var $btnBounce = $('.btn-bounce');
-var $circle = $('.circle');
+var $ball = $('.ball');
+
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
 
 $btnShowHide.on('click', function() {
-    $box.toggleClass('js-box-show-hide');
-});
+    $box.toggleClass('js-show-hide');
+})
     
 $btnMove.on('click', function() {
-    $diamond.toggleClass('js-box-move-inout');
-});
+    $diamond.toggleClass('js-move');
+})
     
-$btnCollapse.on('click', function() {
-    $panel.toggleClass('js-box-collapse');
-});
+$btnExpand.on('click', function() {
+    $panel.toggleClass('js-expand');
+})
     
 $btnBounce.on('click', function() {
-    $circle.addClass('js-circle-bounce');
-});
+    $ball.addClass('js-ball-bounce');
+})
 
-$circle.on('webkitAnimationEnd animationend', function() {
-    $circle.removeClass('js-circle-bounce');
-});
+$ball.on('webkitAnimationEnd animationend', function() {
+    $ball.removeClass('js-ball-bounce');
+})
