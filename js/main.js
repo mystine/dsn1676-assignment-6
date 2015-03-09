f06f40b83334c67dd97120e562b958e774e396f6
Mystine Laframboise Vien <mystinelaframboisevien@Mystines-MacBook-Pro.local>
2015-03-09 13:21:40 -0400

    fixed errors js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var $btnShowHide = $('.btn-show-hide');
 var $box = $('.box');
              
-var $btnMove = $('.btn-move)';
+var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
 
-var $btnCollapseExpand = $('.btn-collapse-expand');
+var $btnExpand = $('.btn-expand');
 var $panel = $('.panel');
 
 var $btnBounce = $('.btn-bounce');
@@ -13,28 +13,28 @@ var $circle = $('.circle');
 var $btnAppend = $('.btn-append');
 var $list = $('.list');
 
-$btnShowHide.on('click', function () {
-    $box.toggleClass('js-box-disapear;);
+$btnShowHide.on('click', function() {
+    $box.toggleClass('js-show-hide');
 });
     
-$btnMove.on('click', function () {
-    $diamond.toggleClass('js-diamond-move');
+$btnMove.on('click', function() {
+    $diamond.toggleClass('js-move');
 });
     
-$btnCollapseExpand.on('click', function () {
-    $panel.toggleClass('js-panel-expand');
+$btnExpand.on('click', function() {
+    $panel.toggleClass('js-expand');
 });
     
-$btnBounce.on('click', function () {
+$btnBounce.on('click', function() {
     $circle.addClass('js-circle-bounce');
 });
 
-$circle.on('animationend', function () {
+$circle.on('webkitAnimationEnd animationend', function() {
     $circle.removeClass('js-circle-bounce');
 });
     
-$btnAppend.on('click', function () {
+$btnAppend.on('click', function() {
     var $li = $('<li>New List Item</li>');
     $list.prepend($li);
-    $li.addClass('js-list-append');
+    $li.addClass('js-li-append');
 });
